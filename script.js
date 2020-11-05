51e45a5316501725b4241219ea11f59afd3f6920
aditi9012 <aditi.1721it1011@kiet.edu>
2020-11-05 16:33:43 +0530

    initial

--- script.js
+++ script.js
@@ -1,25 +1,86 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function myMove() {
+  let elem = document.getElementById("animate");   
+  let pos = 0;
+  let a=Date.now();
+  let id = setInterval(frame, 1000/60);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+      elem.innerHTML=`finished at ${(Date.now()-a)/1000}`;
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+    
+    }
+
   }
-  
-  // Close the dropdown if the user clicks outside of it
-  onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+}
+
+function myMove1() {
+  let elem = document.getElementById("animate1");   
+  let pos = 0;
+  let a=Date.now();
+  requestAnimationFrame(frame);
+  function frame() {
+    if (pos == 350) {
+     
+      elem.innerHTML=`finished at ${(Date.now()-a)/1000}`;
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+     
+      requestAnimationFrame(frame);
     }
   }
+}
 
-  function myFunction(){
-      let x=document.getElementById("mySelect").value;
-      if(x==='setInterval'){
-        
-      }
+function myMove2() {
+  let elem = document.getElementById("animate2");   
+  let pos = 0;
+  let a=Date.now();
+  setTimeout(frame, 1000/60);
+  function frame() {
+    if (pos == 350) {
+      clearTimeout(frame);
+      elem.innerHTML=`finished at ${(Date.now()-a)/1000}`;
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+      setTimeout(frame, 1000/60);
+    }
   }
-  
+}
+
+function myMove3() {
+  let elem = document.getElementById("animate3");   
+  let pos = 0;
+  let a=Date.now();
+  Promise.resolve().then(frame);
+  function frame() {
+    if (pos == 350) {
+     
+      elem.innerHTML=`finished at ${(Date.now()-a)/1000}`;
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+      Promise.resolve().then(frame);
+    }
+  }
+}
+
+function myMove4() {
+  let elem = document.getElementById("animate4");   
+  let pos = 0;
+  let a=Date.now();
+  queueMicrotask(frame);
+  function frame() {
+    if (pos == 350) {
+    
+      elem.innerHTML=`finished at ${(Date.now()-a)/1000}`;
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+      queueMicrotask(frame);
+    }
+  }
+}
